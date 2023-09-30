4d0d15998659aad38ae5611542ab852eebb641a2
Le Minh Khoa <khoab1906498@student.ctu.edu.vn>
2023-10-01 02:22:44 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+
+//handle 404 response
+app.use((req, res, next) =>{
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) =>{
+    return res.status(Error.statusCode || 500).json({
+        message: Error.message || "Internal Server Error",
+    });
+});
 
 app.get("/", (req, res) =>{
     res.json({ message: "Welcome to contact book application."});
